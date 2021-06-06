c33196e9b108c1ff93a1e222062a4755f6d80c3e
kanttanhed <plinio.salazar@yahoo.com.br>
2021-06-06 14:18:36 -0300

    Connect the add contact form to server

--- crm/client/src/components/AddContacts.js
+++ crm/client/src/components/AddContacts.js
@@ -1,30 +1,51 @@
-import React, {Components } from 'react';
+import React, { Component } from 'react';
 
 export default class AddContacts extends Component {
+    submitContact(event){
+        event.preventDefault();
+        
+        let contact = {
+            firstName: this.ref.firstName.value,
+            lastName: this.ref.lastName.value,
+            email: this.ref.email.value
+        }
+
+            fetch("http://localhost:8080/api/contacts", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify(contact),
+            })
+            .then(response => response.json());
+            window.location.reload();
+        
+    }
+
     render() {
         return(
             <div class="row">
-                <form class="col s12">
+                <form class="col s12" onSubmit={this.submitContact.bind(this)}>
                 <div class="row">
                     <div class="input-field col s6">
-                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                    <label for="first_name">First Name</label>
+                    <input placeholder="Placeholder" ref="first_name" type="text" class="validate" />
+                    <label htmlFor="firstName">First Name</label>
                     </div>
 
                     <div class="input-field col s6">
-                    <input id="last_name" type="text" class="validate">
-                    <label for="last_name">Last Name</label>
+                    <input ref="last_name" type="text" class="validate" />
+                    <label htmlFor="lastName">Last Name</label>
                     </div>
                 </div>
     
                 <div class="row">
                     <div class="input-field col s12">
-                    <input id="email" type="email" class="validate">
-                    <label for="email">Email</label>
+                    <input ref="email" type="email" class="validate" />
+                    <label htmlFor="email">Email</label>
                     </div>
                 </div>
                 <div class="row">
-                    
+                    <button className="waves-effect waves-ligth btn" type="submit" name="action">Submit</button>
                 </div>
                 </form>
             </div>
